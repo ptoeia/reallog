1060f32262625f97f71c59f61bbbaa1279f1722a
nana <513993234@qq.com>
2016-12-26 13:27:58 +0800

    cahnge

--- reallog/consumers.py
+++ reallog/consumers.py
@@ -12,13 +12,15 @@ def ws_connect(message):
     response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
 
     #for chunk in AsgiHandler.encode_response(response):
-    message.reply_channel.send(response)
+    #message.reply_channel.send(response)'''
 
 #将发来的信息原样返回
 def ws_message(message):
+    
     message.reply_channel.send({
         "text": message.content['text'],
     })
 #断开连接时发送一个disconnect字符串，当然，他已经收不到了
 def ws_disconnect(message):
-    message.reply_channel.send({"disconnect"})
+    pass
+    #message.reply_channel.send({"disconnect"})
